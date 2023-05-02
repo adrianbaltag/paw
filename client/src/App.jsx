2d44761902b844a16332edda344e19cf5fec63c7
Adrian Baltag <fbaltag83@gmail.com>
2023-05-02 14:35:58 -0400

    added mapbox api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,23 @@ import { useMap } from "react-leaflet/hooks";
 import { Marker } from "react-leaflet/Marker";
 import { Popup } from "react-leaflet/Popup";
 import Community from "./components/Community";
+import { useEffect } from "react";
+
 function App() {
+  const keyIP = import.meta.env.VITE_IPGEOLOCATION_API;
+  console.log(keyIP);
+  useEffect(() => {
+    fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${keyIP}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="parent-container">
       <Navbar />
       <div className="containerMapAndCommunity  w-screen flex justify-evenly">
         <div className="mapContainer w-9/12" style={{ marginTop: "2em" }}>
           <MapContainer
-            center={[40.851185, -73.970163]}
+            center={[40.834285, -73.98123]}
             zoom={20}
             scrollWheelZoom={false}
             style={{ height: "70vh", width: "100%" }}
@@ -21,7 +30,7 @@ function App() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[40.851185, -73.970163]}>
+            <Marker position={[40.834285, -73.98123]}>
               <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
